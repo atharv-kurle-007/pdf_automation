37a2e2d3b3a181e1e7729f4cb98234fcfe885e01
atharv-kurle-007 <akurle402@gmail.com>
2026-07-19 17:12:25 +0530

    initial commit

---
+++ pdf_file_processor_resume.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import pymupdf
+import smtplib
+from email.message import EmailMessage
+
+pdf_folder = Path.home() / "Documents/PDF"
+
+all_pdf = []
+
+# store names & pages in the list
+for pdf in pdf_folder.iterdir():
+    if pdf.suffix == ".pdf":
+        reader = pymupdf.open(pdf)
+        pdf_info = {
+            "name": pdf.name,
+            "pages": len(reader)
+        }
+        all_pdf.append(pdf_info)
+
+# store the list data into the csv file
+file = open("pdf_summary.csv", "w")
+file.write("File name,pages\n")
+
+for pdf in all_pdf:
+    file.write(pdf["name"] + "," + str(pdf["pages"]) + "\n")
+
+file.close()
